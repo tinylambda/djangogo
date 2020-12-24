a49d1a100e3822e427d922bc47605923a9793873
Felix Pan <tinylambda@gmail.com>
2020-12-25 00:33:18 +0800

    Add models for km (means Knowledge management)

--- km/admin.py
+++ km/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Concept
+from .models import Attr
+
+
+class AttrInline(admin.TabularInline):
+    model = Attr
+    extra = 0
+    fields = ('name_readable', 'name', 'options')
+    ordering = ('time_added', )
+    show_full_result_count = True,
+    show_change_link = True
+
+
+@admin.register(Concept)
+class ConceptAdmin(admin.ModelAdmin):
+    inlines = [
+        AttrInline,
+    ]
+    fields = ('name_readable', 'name', 'is_a', 'is_abstract')
+    list_display = ('name_readable', 'name', 'is_a', 'is_abstract')
+    search_fields = ('name_readable', 'name', 'is_a__name_readable')
+    ordering = ('is_abstract', 'time_added', )
+    show_full_result_count = True
+
--- km/models.py
+++ km/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from .defaults import ATTR_OPTIONS_DEFAULT
+
+
+class Concept(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=128, unique=True)
+    name_readable = models.CharField(verbose_name='Readable name', max_length=128, unique=True)
+    is_abstract = models.BooleanField(default=False)
+    is_a = models.ForeignKey('Concept', verbose_name='Parent Concept', on_delete=models.CASCADE, blank=True, null=True)
+    time_added = models.DateTimeField(verbose_name="Time Added", auto_now_add=True)
+    time_updated = models.DateTimeField(verbose_name="Time Updated", auto_now=True)
+
+    def __str__(self):
+        return f'' \
+               f'{self.name} ' \
+               f'is {"a root concept" if not self.is_a else self.is_a.name}' \
+               f'that {"can be initialized" if not self.is_abstract else "cannot be initialized"}'
+
+
+class Attr(models.Model):
+    concept = models.ForeignKey(Concept, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Name', max_length=128, unique=False)
+    name_readable = models.CharField(verbose_name='Readable name', max_length=128, unique=False)
+    options = models.TextField(verbose_name='Attr options', default=ATTR_OPTIONS_DEFAULT, blank=True, null=True)
+    time_added = models.DateTimeField(verbose_name="Time Added", auto_now_add=True)
+    time_updated = models.DateTimeField(verbose_name="Time Updated", auto_now=True)
+
+    class Meta:
+        unique_together = ('concept', 'name')
+
